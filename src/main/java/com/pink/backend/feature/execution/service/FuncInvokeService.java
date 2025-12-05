b24da49d3ca500ce63556aecca44a0bbd5a708d3
noeyah2 <hy0708@cau.ac.kr>
2025-12-06 00:47:10 +0900

    feat: FuncCreateService, FuncInvokeService, SqsMessageSender 에 로그 추가
    SQS 정확한 확인 전 진행을 위한 로그 추가

--- src/main/java/com/pink/backend/feature/execution/service/FuncInvokeService.java
+++ src/main/java/com/pink/backend/feature/execution/service/FuncInvokeService.java
@@ -11,6 +11,7 @@ import com.pink.backend.global.exception.CustomException;
 import com.pink.backend.global.exception.ErrorCode;
 import com.pink.backend.global.sqs.SqsMessageSender;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronization;
@@ -18,6 +19,7 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FuncInvokeService {
@@ -28,6 +30,7 @@ public class FuncInvokeService {
 
     @Transactional
     public FuncInvokeRes invokeFunction(UUID functionId) {
+        log.info("함수 실행을 요청합니다. 함수 ID: {}", functionId);
 
         Function function = functionRepository.findById(functionId)
                 .orElseThrow(() -> new CustomException(ErrorCode.FUNCTION_NOT_FOUND));
@@ -48,6 +51,8 @@ public class FuncInvokeService {
             @Override
             public void afterCommit() {
                 sqsMessageSender.sendMessage(message);
+                log.info("SQS에 실행 요청을 성공적으로 보냈습니다. 함수 ID: {}, 실행 ID: {}",
+                        message.getFunctionId(), message.getExecutionId());
             }
         });
 
