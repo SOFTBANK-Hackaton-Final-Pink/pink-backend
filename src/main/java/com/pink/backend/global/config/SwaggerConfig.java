dd110b6a7a791fc56d813a5bf9bbb6c6cd64cc4e
1026hz <1026hzz@gmail.com>
2025-12-06 18:38:05 +0900

    fix: SwaggerConfig

--- src/main/java/com/pink/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/pink/backend/global/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.pink.backend.global.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,12 +15,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Team Pink API")
-                        .version("1.0.0")
-                        .description("Serverless Function Management Platform")
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+            .servers(List.of(
+                new Server().url("https://api.code-bento.shop")
+            ))
+            .info(new Info()
+                .title("Team Pink API")
+                .version("1.0.0")
+                .description("Serverless Function Management Platform")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
     }
 }
